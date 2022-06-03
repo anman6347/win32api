fe4e6c1d20dece4b7d20724bd4e41a9a7c96dc22
anman6347 <4620096@ed.tus.ac.jp>
2022-06-03 23:58:18 +0900

    :tada: initial commit

---
+++ sec1_1/main.cpp
@@ -0,0 +1,76 @@
+#include <Windows.h>
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMessage, WPARAM wParam, LPARAM lParam)
+{
+    static int count = 0;
+    PAINTSTRUCT ps;
+    WCHAR testMsg[4096];
+
+    switch(uMessage) {
+        case WM_PAINT:
+            BeginPaint(hWnd, &ps);
+            wsprintf(testMsg, L"左クリック = %d", count);
+            TextOut(ps.hdc, 10, 20, testMsg, wcslen(testMsg));
+            EndPaint(hWnd, &ps);
+            break;
+        
+        case WM_LBUTTONDOWN:
+            count++;
+            InvalidateRect(hWnd, NULL, TRUE);
+            break;
+
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+
+        default:
+            return DefWindowProc(hWnd, uMessage, wParam, lParam);    
+    }
+    return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                                        LPSTR lpCmdLine, int nCmdShow)
+{
+    WNDCLASSEX wcx;
+    MSG msg;
+    HWND hWnd;
+    LPCWSTR szTitle = L"サンプル";
+    LPCWSTR szWindowClass = L"SAMPLE";
+
+    ZeroMemory(&wcx, sizeof(wcx));
+
+    wcx.cbSize = sizeof(WNDCLASSEX);
+    wcx.style = 0;
+    wcx.lpfnWndProc = WndProc;
+    wcx.cbClsExtra = 0;
+    wcx.cbWndExtra = 0;
+    wcx.hInstance = hInstance;
+    wcx.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wcx.hIconSm = LoadIcon(NULL, IDI_WINLOGO);
+    wcx.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wcx.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+    wcx.lpszMenuName = NULL;
+    wcx.lpszClassName = szWindowClass;
+
+    if (!RegisterClassEx(&wcx)){
+        return 0;
+    }
+
+    hWnd = CreateWindow(szWindowClass, szTitle,
+                        WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+                        CW_USEDEFAULT, CW_USEDEFAULT,
+                        400, 100,
+                        HWND_DESKTOP, NULL, hInstance, NULL);
+    
+    if (!hWnd) {
+        return 0;
+    }
+
+    while(GetMessage(&msg, NULL, 0, 0) != 0) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return msg.wParam;
+}
